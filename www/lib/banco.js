432df79f8672c8c4fb8daff6401bf59b26d7df47
rick43255 <henrique.souza492@gmail.com>
2020-06-16 08:58:55 +0900

    Primeiro vídeo

--- www/lib/banco.js
+++ www/lib/banco.js
@@ -1,2 +1,24 @@
-// This is a JavaScript file
+$(document).on("click","#salvar",function(){
+  var parametros= {
+    "nome":$("#nome").val(),
+    "email":$("#email").val(),
+    "senha":$("#senha").val()
+  }
+  $.ajax({
+    type:"post",//como enviar os dados
+    url:"https://wordpress-online-2.000webhostapp.com/webservice/cadastra.php",//para onde eviar
+    data:parametros, //o que vou eviar
+    //se der certo
+    success:function(data){
+      navigator.notification.alet(data);
+      $("#nome").val(""),
+      $("#email").val(""),
+      $("#senha").val("")
+    },
 
+    //se der errado
+    error:function(data){
+      navigator.notification.alet("Erro no cadstro!!!");
+    }
+  });
+});
